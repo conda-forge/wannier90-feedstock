2916d3e5b7c1ac2758bb9ba492075f76205d386d
Marnik Bercx <mbercx@gmail.com>
2026-03-20 11:10:12 +0100

    🔧 `build.sh`: skip tests for cross-compilation builds
    When cross-compiling `osx-arm64` on an `x86_64` host (`osx_64`), the compiled binaries
    cannot be executed on the build host. On Linux, QEMU via `binfmt_misc` handles this, but
    the tests run very slowly and some fail due to small discrepancies in results.
    
    Here we skip the test suite invocation for cross-compilation via a check on
    `CONDA_BUILD_CROSS_COMPILATION`.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp ${RECIPE_DIR}/make.inc ${SRC_DIR}/make.inc
 make wannier -j$CPU_COUNT
-make test-serial -j$CPU_COUNT
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
+  make test-serial -j$CPU_COUNT
+fi
 make install PREFIX=${PREFIX}
